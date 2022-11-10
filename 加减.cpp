b9afcd6f2d090502d4ce22a0099691c48ca41be0
the Giant of Heaven <112753773+2807338@users.noreply.github.com>
2022-11-10 14:48:39 +0800

    Add files via upload

---
+++ 加减.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main()
+{
+	int iData1,iData2;
+	printf("请输入两个整数："); 
+	scanf("%d%d", &iData1,&iData2);
+	if( iData1>iData2)
+	{
+		printf("两个数中的最大值是： %d\n",iData1);
+	 } else
+	 {
+	    printf("两个数中的最大值是： %d\n",iData1);
+	 }
+}
